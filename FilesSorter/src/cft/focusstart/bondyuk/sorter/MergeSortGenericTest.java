1c024061947fc8be3c424a07d9936cd6d3a0ce0f
Aleksander Bondyuk <aleksander.bondyuk@yandex.ru>
2020-02-10 17:01:07 +0700

    Обновление (причесывание) теста сортировки

--- FilesSorter/src/cft/focusstart/bondyuk/sorter/MergeSortGenericTest.java
+++ FilesSorter/src/cft/focusstart/bondyuk/sorter/MergeSortGenericTest.java
@@ -8,21 +8,20 @@ public class MergeSortGenericTest {
         Comparator ascendingComparator = new ComparatorImp(SortOrder.ASCENDING);
 
         String[] arrayOfStrings = {"Andree", "Leana", "Faviola", "Loyce", "Quincy", "Milo", "Jamila", "Toccara", "Nelda", "Blair", "Ernestine", "Chara", "Kareen", "Monty", "Rene", "Cami", "Winifred", "Tara", "Demetrice", "Azucena"};
-        Sorter stringSorter = new MergeSortGeneric();
+        Sorter sorter = new MergeSortGeneric();
 
-        stringSorter.sort(arrayOfStrings, 0, arrayOfStrings.length - 1, descendingComparator);
+        sorter.sort(arrayOfStrings, 0, arrayOfStrings.length - 1, descendingComparator);
         System.out.println(java.util.Arrays.toString(arrayOfStrings));
 
-        stringSorter.sort(arrayOfStrings, 0, arrayOfStrings.length - 1, ascendingComparator);
+        sorter.sort(arrayOfStrings, 0, arrayOfStrings.length - 1, ascendingComparator);
         System.out.println(java.util.Arrays.toString(arrayOfStrings));
 
         Integer[] arrayOfIntegers = {10, 9, 8, 7, 5, 4, 3, 6, 1, 2, 3};
-        Sorter integerSorter = new MergeSortGeneric();
 
-        integerSorter.sort(arrayOfIntegers, 0, arrayOfIntegers.length - 1, descendingComparator);
+        sorter.sort(arrayOfIntegers, 0, arrayOfIntegers.length - 1, descendingComparator);
         System.out.println(java.util.Arrays.toString(arrayOfIntegers));
 
-        integerSorter.sort(arrayOfIntegers, 0, arrayOfIntegers.length - 1, ascendingComparator);
+        sorter.sort(arrayOfIntegers, 0, arrayOfIntegers.length - 1, ascendingComparator);
         System.out.println(java.util.Arrays.toString(arrayOfIntegers));
     }
 }
